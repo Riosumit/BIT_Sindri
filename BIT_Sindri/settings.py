87153bb8bce6142f54df8f2126422731dccc1cac
Riosumit <rajsumit22032003@gmail.com>
2022-12-24 14:41:57 +0530

    prepare to deploy

--- BIT_Sindri/settings.py
+++ BIT_Sindri/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*_)jihpv%5)khwc-!$8((gy9fxw1oy$pjp5qkfnqpzz&pk4r8)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
@@ -120,9 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
  
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
